9a483687f2d7da77a7d12f86ec53cb7fd411156d
roelandheerema <roelandheerema@hotmail.com>
2022-12-03 01:16:40 +0100

    Update BEC_Compute_AUC.m

--- BEC_Compute_AUC.m
+++ BEC_Compute_AUC.m
@@ -1,4 +1,4 @@
-function [AUC] = BEC_Compute_AUC(muPhi)
+function [AUC] = BEC_Compute_AUC(muPhi,costlevels_per_bin)
 % Calculate area under the curve of value function
 %   input: muPhi, the parameter posteriors from the online trial generation (OTG) algorithm
 %   output: AUC, the area under the curve
@@ -8,10 +8,14 @@ function [AUC] = BEC_Compute_AUC(muPhi)
         AUC = NaN;
         return
     end
+    if ~exist('costlevels_per_bin','var')
+        costlevels_per_bin = 10; %default
+    elseif ~isdouble(costlevels_per_bin)
+        error('"n_levels" must be a variable of type double')
+    end
 % Settings
     nbins = length(muPhi)-1;
     binlimits = 0:1/nbins:1;
-    costlevels_per_bin = 10; %default setting
     AUC = 0;
 % Loop through cost bins
     for bin = 1:nbins %Loop through cost bins        
@@ -27,8 +31,9 @@ function [AUC] = BEC_Compute_AUC(muPhi)
                 C_i = binlimits(bin+1); %Cost level of the bin edge
                 R_i = 1 - k*C_i - b; %Indifference reward level at cost bin edge 
         %Compute area under the curve (positive range only)
-            X = linspace(binlimits(bin),binlimits(bin+1),costlevels_per_bin);
+            X = linspace(binlimits(bin),binlimits(bin+1),costlevels_per_bin+1);
             Y = 1 - b - k.*X;
-            AUC = AUC + trapz(Y(Y>0));
+            inc = diff(X(1:2));
+            AUC = AUC + trapz(Y(Y>0))*inc;
     end
 end %function
